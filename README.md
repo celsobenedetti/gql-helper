d8aaf0fcf9e2d6331f4ad7a35b5203614054580d
Celso Benedetti <celsobenedetti2@gmail.com>
2024-06-04 11:13:43 -0300

    feat: read file

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # GraphQL Helper
 
 A sandbox project to hack around utils to convert `query.gql` files into HTTP requests.
+
+1. Read `query.gql` file
+2. Read `vars.json` file
+3. Make HTTP request with correct headers, data and vars
+
+## Improvements
+
+- [ ] Read folder with `.gql` files and select which query to run
--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+var TOKEN = os.Getenv("TOKEN")
+var QUERY_FILE = os.Getenv("QUERY_FILE")
+var VARS_FILE = os.Getenv("VARS_FILE")
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	query, err := readQueryAndVars()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(query)
+
+	return nil
+}
+
+func readQueryAndVars() (*Query, error) {
+	query, err := os.ReadFile(QUERY_FILE)
+	if err != nil {
+		return nil, err
+	}
+
+	vars, err := os.ReadFile(VARS_FILE)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Query{
+		query: string(query),
+		vars:  string(vars),
+	}, nil
+}
+
+type Query struct {
+	query string
+	vars  string
 }
